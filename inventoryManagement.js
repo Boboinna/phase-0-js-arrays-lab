c9fb06ea9bac33b2760a9399ec6343a05e0c08d8
Wambui <andrelle364808@gmail.com>
2026-06-21 20:46:25 +0300

    Done!

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,32 @@
 // Write your code here
 
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+function logFirstProduct(product) {
+  console.log(products[0]);
+}
+logFirstProduct(products);
+
+function addProduct(product) {
+  products.push(product);
+}
+
+addProduct("Space buds");
+console.log(products);
+
+function updateProductName(index, name) {
+  products[index] = name;
+}
+
+updateProductName(0, "Tablet");
+console.log(products);
+
+function removeLastProduct(product) {
+  products.pop();
+}
+
+removeLastProduct(products);
+console.log(products);
 
 // Export the necessary parts for testing
 module.exports = {
